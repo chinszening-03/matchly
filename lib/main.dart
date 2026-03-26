1d91600843bfbca99dd5459594028f8aa49a21bf
chinsn2003 <cchi0098@student.monash.edu>
2026-03-26 22:05:36 +0800

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,22 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:matchly/firebase_options.dart';
 import 'screens/auth_wrapper.dart';
+import 'dart:io' show Platform;
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
- 
+  
+  if (Platform.isAndroid) {
+    final GoogleMapsFlutterPlatform mapsImplementation = GoogleMapsFlutterPlatform.instance;
+    if (mapsImplementation is GoogleMapsFlutterAndroid) {
+      mapsImplementation.useAndroidViewSurface = true; 
+    }
+  }
+
   runApp(const MyApp());
 
 }
